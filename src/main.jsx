11874251b77654b035d24aa0abb627971cd829f7
kaplack <95725401+kaplack@users.noreply.github.com>
2026-09-25 19:56:09 -0500

    Update src/main.jsx for enrollment demo

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import ColoresYSonrisas from './pages/ColoresYSonrisas'
+import DashboardDemo from './pages/DashboardDemo'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/demos/colores-y-sonrisas" element={<ColoresYSonrisas />} />
+        <Route path="/demos/colores-y-sonrisas/admin" element={<DashboardDemo />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
